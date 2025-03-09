509236c593b302d9eee3f6a841a2dca7305b9643
gramble69 <oscarpersonal886@gmail.com>
2025-03-09 09:23:31 -0500

    Added stuff from before cuz I'm only now using git.(part 4)

---
+++ main/maps/train_1MapEvents.gd
@@ -0,0 +1,15 @@
+extends Node3D
+signal canOpenDoors
+@onready var anim = $mapEvents
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	anim.play("wakeUp")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_map_events_animation_finished(anim_name: StringName) -> void:
+	emit_signal("canOpenDoors")
