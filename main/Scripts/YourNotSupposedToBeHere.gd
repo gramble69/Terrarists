e234cdf8dfdf3add9f849f9c9f8ec9376f52689c
gramble69 <oscarpersonal886@gmail.com>
2025-09-17 17:40:04 -0500

    updated from godot 4.4 to 4.5 which has a lot of cool features so yeah thats cool. fixed visible player in wake up scene made your not supposed to be here less absolute shit and more doesn't make me feel embarrassed every time i play the game.

--- main/Scripts/YourNotSupposedToBeHere.gd
+++ main/Scripts/YourNotSupposedToBeHere.gd
@@ -1,16 +1,22 @@
 extends Area3D
 
+@onready var delay = $Timer
 @onready var yourNotSupposedToBeHere = $AudioStreamPlayer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+var delayStarted
 
 func _on_player_entered_alley_way() -> void:
+	print("delayStarted")
+	delay.start()
+	delayStarted = true
+
+#func _process(delta: float) -> void:
+#	if (delayStarted == true):
+#		if (delay.time_left == 0.01):
+#			print("delayFinished")
+#			yourNotSupposedToBeHere.play(0.0)
+# that is bassicly what the _on_timer_timeout function does
+
+func _on_timer_timeout() -> void:
+	print("delayFinished")
 	yourNotSupposedToBeHere.play(0.0)
